ec39926ed3e079437e884e5a80bc7c05f375de10
AndrewO13 <romieandy@gmail.com>
2026-04-13 13:24:41 +0300

    Refactor comments and formatting in serializers and services for consistency

--- backend/movies/serializers.py
+++ backend/movies/serializers.py
@@ -20,9 +20,9 @@ class PersonCompactSerializer(serializers.ModelSerializer):
         model = Person
         fields = ["id", "tmdb_id", "name", "profile_url", "known_for_department"]
         
-  def _get_release_year(obj):
-    """Extract the release year from a model instance (shared by compact/detail serializers)."""
-    return obj.release_date.year if obj.release_date else None
+    def _get_release_year(obj):
+        """Extract the release year from a model instance (shared by compact/detail serializers)."""
+        return obj.release_date.year if obj.release_date else None
 
 
 class PersonDetailSerializer(serializers.ModelSerializer):
@@ -132,8 +132,8 @@ class TMDBMovieSerializer(serializers.Serializer):
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
-       from django.conf import settings
-# ... inside method:
+        from django.conf import settings
+    # ... inside method:
         image_base = settings.TMDB_IMAGE_BASE_URL
 
         if data.get("poster_path"):
